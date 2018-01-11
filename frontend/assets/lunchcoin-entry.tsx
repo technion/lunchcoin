127d8dfd2b40fed80b3f557f4b603a5c1d1abbfa
Technion <technion@lolware.net>
2018-01-11 09:15:37 +0000

    Start of an interactive submission form.

--- frontend/assets/lunchcoin-entry.tsx
+++ frontend/assets/lunchcoin-entry.tsx
@@ -1,17 +1,25 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-export class App extends React.Component<{}, {}> {
+interface LunchFormProps {
+  onOrderSubmit: (data: string) => void;
+}
+
+
+class LunchForm extends React.Component<LunchFormProps, {}> {
+  constructor(props: LunchFormProps) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e: any) {
+    e.preventDefault();
+    this.props.onOrderSubmit("new order");
+  }
+
   public render() {
     return (
-      <div className="column">
-    <h2 className="ui teal image header">
-      <img src="assets/images/logo.png" className="image" />
-      <div className="content">
-        Enter your order details
-      </div>
-    </h2>
-    <form className="ui large form" method="post" action="/api/new">
+    <form className="ui large form" method="post" action="/api/new" onSubmit={this.handleSubmit}>
       <div className="ui stacked segment">
         <div className="field">
           <div className="ui left icon input">
@@ -28,6 +36,58 @@ export class App extends React.Component<{}, {}> {
         <input type="submit" className="ui fluid large teal submit button" value="Order" />
       </div>
     </form>
+    );
+  }
+
+}
+
+interface MainComponentState {
+  showData: "form" | "mining";
+}
+
+class MainContent extends React.Component<{}, MainComponentState> {
+  constructor(props: {}) {
+    super(props);
+    this.submitOrder = this.submitOrder.bind(this);
+    this.state = {
+      showData: "form"
+    };
+  }
+
+  submitOrder(data: string) {
+    this.setState({...this.state, showData: "mining" });
+  }
+
+  public render() {
+    let renderdata;
+    switch(this.state.showData) {
+      case "form":
+      renderdata = <LunchForm onOrderSubmit={this.submitOrder} />;
+      break;
+      case "mining":
+      renderdata = <Mining>
+      break;
+      default:
+      console.error("Major failure");
+    }
+
+    return (
+      <div> { renderdata } </div>
+    )
+  }
+}
+
+export class App extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div className="column">
+    <h2 className="ui teal image header">
+      <img src="assets/images/logo.png" className="image" />
+      <div className="content">
+        Enter your order details
+      </div>
+    </h2>
+    <MainContent />
     <div className="ui message">
       <a href="/api/orders">Looking for today's orders? Click Here</a>
     </div>
